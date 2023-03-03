02bb763c9a2ab99f1b3b4a9196abbfa5e47c7c77
gaetan pecqueux <gaetan.pecqueux@gmail.com>
2023-03-03 15:04:34 +0100

    🌈 last Gaston commit

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -7,22 +7,22 @@ module OrdersHelper
     shipping_date = (order.created_at + shipping_days.days).strftime("%d/%m/%Y")
 
     if order.status == status
-      content_tag :div, class: "order_show col-4" do
+      content_tag :div, class: "order_show col-4 #{order.status}" do
         concat content_tag(:h1, "Order ##{(rand()*1000000).to_i} - #{order.id}")
-        concat content_tag(:p, "NOM : #{content_tag(:span, order.drug.name)}")
+        concat content_tag(:p, "NOM : #{order.drug.name}")
         concat content_tag(:p, "####################################")
         concat content_tag(:p, "DEALER : #{order.drug.user.username}")
         concat content_tag(:p, "STATUT DE LA COMMANDE : #{order.status}")
         concat content_tag(:br)
         concat content_tag(:p, "DATE DE COMMANDE : #{ddc}")
-        concat content_tag(:p, "CATEGORY : #{content_tag(:span, order.drug.category)}")
+        concat content_tag(:p, "CATEGORY : #{order.drug.category}")
         concat content_tag(:p, "####################################")
         concat content_tag(:p, order.drug.description)
         concat content_tag(:p, "####################################")
         concat content_tag(:p, "DATE DE RECEPTION : #{shipping_date}")
         concat content_tag(:p, "####################################")
         concat content_tag(:p, "MONTANT : ")
-        concat content_tag(:p, content_tag(:span, "#{order.drug.price}€"), class: "price")
+        concat content_tag(:p, "#{order.drug.price}€", class: "price")
         if current_user.role == "buyer"
           if order.status == "pending"
             concat render(partial: "orders/actions_btn", locals: {order: order, delete: true})
