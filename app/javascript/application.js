89314a3039216d43f48abc0b486a2f1ceaae1ba8
gaetan pecqueux <gaetan.pecqueux@gmail.com>
2023-03-03 08:19:19 +0100

    🎨 re-pre-re-font

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,26 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
+
+
+// TROLL //
+console.log("# 🔐 KEY : dHr2O042x4LGMN #");
+console.log(" ");
+console.log('%c!!! Attention !!!', 'color: red; font-weight: bold; font-size: 30px;');
+console.log(" ");
+console.log("###########################");
+console.log('%cNous avons détecté une activité suspecte sur votre appareil.', 'color: #FFF; font-weight: bold; font-size: 18px;');
+console.log('%cNous vous rappelons qu\'il est important de rester vigilant en ligne pour protéger vos informations personnelles.', 'color: #FFF; font-weight: bold; font-size: 16px;');
+console.log('%cSi vous avez besoin d\'aide, n\'hésitez pas à contacter la police au 4242.', 'color: #FFF; font-weight: bold; font-size: 16px;');
+console.log("###########################");
+console.log('%cPour plus d\'informations sur la sécurité sur Internet, veuillez consulter notre site Web dédié.', 'color: #FFF; font-size: 14px;');
+console.log("###########################");
+console.log(" ");
+console.log(" ");
+console.log("# 🔐 KEY : JSjf87d42d4qdf #");
+console.log('%cNous allons maintenant récupérer vos informations bancaires.', 'color: green; font-size: 12px;');
+console.log('%cRécupération de vos information banquaire ...', 'color: green; font-size: 12px;');
+console.log('%cRécupération de vos information banquaire ...', 'color: green; font-size: 12px;');
+console.log('%cCarte : 1245 0203 3254 4554 || Date : 03.03.25 || Crypto : 420', 'color: green; font-size: 12px;');
+console.log('%cTerminé. Merci 👍', 'color: green; font-size: 12px;');
+// END TROLL //
